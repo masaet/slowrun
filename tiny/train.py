835535cf863fb6564e7cd573ea8a03ab90551873
masaet <masaet@stud.ntnu.no>
2026-03-13 10:10:56 +0100

    fix bfloat16 dtype mismatch in adamw_step_fused for A100 compatibility

--- tiny/train.py
+++ tiny/train.py
@@ -414,6 +414,7 @@ polar_express_coeffs = [
 
 @torch.compile(dynamic=False, fullgraph=True)
 def adamw_step_fused(p, grad, exp_avg, exp_avg_sq, step_t, lr_t, beta1_t, beta2_t, eps_t, wd_t):
+    grad = grad.to(exp_avg.dtype)
     p.mul_(1 - lr_t * wd_t)
     exp_avg.lerp_(grad, 1 - beta1_t)
     exp_avg_sq.lerp_(grad.square(), 1 - beta2_t)
